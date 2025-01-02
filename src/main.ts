b24735526a3810077af87a8deb3b8feb14c9183e
marcdelgado <marcdelgado@gmail.com>
2025-01-02 03:29:11 +0100

    arreglo viewport y 100vh

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,21 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+function adjustHeight() {
+  const dynamicHeightElement = document.getElementsByClassName('fullscreen')[0] as HTMLElement;;
+  if(dynamicHeightElement!=null){
+    if (window.visualViewport) {
+      dynamicHeightElement.style.height = `${window.visualViewport.height}px`;
+    } else {
+      // Fallback para navegadores que no soportan visualViewport
+      dynamicHeightElement.style.height = `${window.innerHeight}px`;
+    }
+  }
+}
+
+// Ajusta la altura al cargar la página y al cambiar el tamaño del viewport
+window.addEventListener('resize', adjustHeight);
+window.addEventListener('orientationchange', adjustHeight);
+adjustHeight(); // Llamada inicial
